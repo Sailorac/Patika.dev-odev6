9ed7d584a693ce04fdbee2657e4063b3fbb79763
Sailorac <119450113+Sailorac@users.noreply.github.com>
2022-12-03 20:42:31 +0300

    Add files via upload

---
+++ SQL-ödev-6.sql
@@ -0,0 +1,25 @@
+--SELECT avg(rental_rate) from film
+
+
+--SELECT COUNT(title) FROM film
+--WHERE title LIKE 'C%';
+
+
+--SELECT max(length) from film
+--where rental_rate = 0.99
+
+
+
+--SELECT count(distinct replacement_cost)  from film
+--where length >  150 
+
+
+ 
+
+
+
+
+
+
+
+
